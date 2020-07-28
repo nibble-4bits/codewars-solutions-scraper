5d5902fa40fbd48310229e6e153481aa8b1cfc7a
Luis De Anda <ldeandac16@gmail.com>
2020-07-27 19:06:35 -0500

    Create extensions.js file
    Exports an object that maps a language name to its default extension

---
+++ src/extensions.js
@@ -0,0 +1,12 @@
+const extensions = {
+    c: 'c',
+    cpp: 'cpp',
+    csharp: 'cs',
+    java: 'java',
+    javascript: 'js',
+    python: 'py',
+    shell: 'sh',
+    typescript: 'ts'
+};
+
+module.exports = extensions;
